0b4d790e38d06be44f5311f4c67ed9131b727bf9
Domo <29755257+Utopianer@users.noreply.github.com>
2023-01-09 19:47:50 +0000

    Add categories to pizzalib and category attribute to Token Metadata .json

--- js/pizzalib.js
+++ js/pizzalib.js
@@ -1,203 +1,203 @@
 window.pizzalib = [
 	{
 		"name": "Stellar Gate",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889710932007850888",
 		"price": 45,
 		"image": "https://www.boredpizzas.com/img/pizzas/stellar-gate.jpg"
 	},
 	{
 		"name": "Authentick",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889709832496219112",
 		"price": 225,
 		"image": "https://www.boredpizzas.com/img/pizzas/authentick.jpg"
 	},
 	{
 		"name": "Parcel Party",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889707633472963560",
 		"price": 225,
 		"image": "https://www.boredpizzas.com/img/pizzas/parcel-party.jpg"
 	},
 	{
 		"name": "Mutant",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889706533961347784",
 		"price": 150,
 		"image": "https://www.boredpizzas.com/img/pizzas/mutant.jpg"
 	},
 	{
 		"name": "Michi Todd",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889693339821802472",
 		"price": 225,
 		"image": "https://www.boredpizzas.com/img/pizzas/michi-todd.jpg"
 	},
 	{
 		"name": "MetaDojo",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889692240310174696",
 		"price": 225,
 		"image": "https://www.boredpizzas.com/img/pizzas/metadojo.jpg"
 	},
 	{
 		"name": "HODLHQ",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889704334938080232",
 		"price": 225,
 		"image": "https://www.boredpizzas.com/img/pizzas/hodlhq.jpg"
 	},
 	{
 		"name": "Waifumon",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889695538845062024",
 		"price": 45,
 		"image": "https://www.boredpizzas.com/img/pizzas/waifumon.jpg"
 	},
 	{
 		"name": "BabyDolls",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889703235426452456",
 		"price": 225,
 		"image": "https://www.boredpizzas.com/img/pizzas/babydolls.jpg"
 	},
 	{
 		"name": "Wilderness",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889705434449709008",
 		"price": 112.5,
 		"image": "https://www.boredpizzas.com/img/pizzas/wilderness.jpg"
 	},
 	{
 		"name": "Infinity",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889702135914923680",
 		"price": 1,
 		"image": "https://www.boredpizzas.com/img/pizzas/infinity.jpg"
 	},
 	{
 		"name": "Metatrekkers",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889713131031102440",
 		"price": 225,
 		"image": "https://www.boredpizzas.com/img/pizzas/metatrekkers.jpg"
 	},
 	{
 		"name": "Halloween",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889712031519474664",
 		"price": 175,
 		"image": "https://www.boredpizzas.com/img/pizzas/halloween.jpg"
 	},
 	{
 		"name": "Festival",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889717529077613544",
 		"price": 175,
 		"image": "https://www.boredpizzas.com/img/pizzas/festival.jpg"
 	},
 	{
 		"name": "HirotoKai",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889718628589240820",
 		"price": 225,
 		"image": "https://www.boredpizzas.com/img/pizzas/hirotokai.jpg"
 	},
 	{
 		"name": "DaddyChang",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889716429565985768",
 		"price": 225,
 		"image": "https://www.boredpizzas.com/img/pizzas/daddychang.jpg"
 	},
 	{
 		"name": "TheHolyOnes",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889719728100869096",
 		"price": 225,
 		"image": "https://www.boredpizzas.com/img/pizzas/theholyones.jpg"
 	},
 	{
 		"name": "OffGridGecko",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889720827612496872",
 		"price": 225,
 		"image": "https://www.boredpizzas.com/img/pizzas/offgridgecko.jpg"
 	},
 	{
 		"name": "Genesis 1",
-		"category": "",
+		"category": "Legacy",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889686742752035816",
 		"price": 24500,
 		"image": "https://www.boredpizzas.com/img/pizzas/genesis-1.jpg"
 	},
 	{
 		"name": "MeasePhotography",
-		"category": "",
+		"category": "Collaboration",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889721927124124648",
 		"price": 225,
 		"image": "https://www.boredpizzas.com/img/pizzas/mease.jpg"
 	},
 	{
 		"name": "Genesis 2",
-		"category": "",
+		"category": "Legacy",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889687842263663592",
 		"price": 20000,
 		"image": "https://www.boredpizzas.com/img/pizzas/genesis-2.jpg"
 	},
 	{
 		"name": "Genesis 3",
-		"category": "",
+		"category": "Legacy",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889688941775291368",
 		"price": 16000,
 		"image": "https://www.boredpizzas.com/img/pizzas/genesis-3.jpg"
 	},
 	{
 		"name": "Genesis 4",
-		"category": "",
+		"category": "Legacy",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889690041286919144",
 		"price": 12500,
 		"image": "https://www.boredpizzas.com/img/pizzas/genesis-4.jpg"
 	},
 	{
 		"name": "Genesis 5",
-		"category": "",
+		"category": "Legacy",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889691140798546920",
 		"price": 9500,
 		"image": "https://www.boredpizzas.com/img/pizzas/genesis-5.jpg"
 	},
 	{
 		"name": "Genesis 6",
-		"category": "",
+		"category": "Legacy",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889694439333430248",
 		"price": 7000,
 		"image": "https://www.boredpizzas.com/img/pizzas/genesis-6.jpg"
 	},
 	{
 		"name": "Genesis 7",
-		"category": "",
+		"category": "Legacy",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889701036403196904",
 		"price": 5000,
 		"image": "https://www.boredpizzas.com/img/pizzas/genesis-7.jpg"
 	},
 	{
 		"name": "Genesis 8",
-		"category": "",
+		"category": "Legacy",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889708732984591336",
 		"price": 3500,
 		"image": "https://www.boredpizzas.com/img/pizzas/genesis-8.jpg"
 	},
 	{
 		"name": "Genesis 9",
-		"category": "",
+		"category": "Legacy",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889714230542730216",
 		"price": 2500,
 		"image": "https://www.boredpizzas.com/img/pizzas/genesis-9.jpg"
 	},
 	{
 		"name": "Genesis 10",
-		"category": "",
+		"category": "Legacy",
 		"tokenid": "15325094160703702961871960386367558677721013205368135024481889715330054357992",
 		"price": 2000,
 		"image": "https://www.boredpizzas.com/img/pizzas/genesis-10.jpg"
